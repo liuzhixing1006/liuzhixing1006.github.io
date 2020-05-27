f9143d8b5c5f56a4b85ad87f54bd6c511eb9c367
“lzh576177775” <a576177775@163.com>
2020-05-27 11:57:41 +0800

    Java问题排查及调优

---
+++ img/doc-pic/2.FlinkStateAndCheckPoint/时间戳(Unix timestamp)转换工具 - 在线工具_files/c2aea8d75f0f79618409608188812fe2.js
@@ -0,0 +1,25 @@
+(function () {
+	/**
+	 * 只能统计 _blank 的点击事件
+	 */
+	$(document).on('click', '.js-track', function (e) {
+		var target = $(this).attr('href');
+		var url = document.URL;
+		var params = {target: target, url: url};
+		var args = "";
+		for (var i in params) {
+			if (args != "") {
+				args += "&";
+			}
+			args += i + "=" + encodeURIComponent(params[i]);
+		}
+		var img = new Image(1, 1);
+		img.src = '//analytics.tool.lu/__ev.gif?' + args;
+
+		var rndId = '_img_' + Math.random();
+		window[rndId] = img;
+		img.onload = img.onerror = function () {
+			window[rndId] = null;
+		};
+	});
+})();
